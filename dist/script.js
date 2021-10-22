aea940cc3ae2a73f25ef5e9ad64183e803f86518
Dmitry <lupanov5@mail.ru>
2021-10-22 15:23:08 +0300

    Created methods for working with the elements: closest, siblings

--- dist/script.js
+++ dist/script.js
@@ -247,6 +247,52 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.find = function (selecto
   }
 };
 
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.closest = function (selector) {
+  let counter = 0;
+
+  for (let i = 0; i < this.length; i++) {
+    this[i] = this[i].closest(selector);
+    counter++;
+  }
+
+  const objLength = Object.keys(this).length;
+
+  for (; counter < objLength; counter++) {
+    delete this[counter];
+  }
+
+  return this;
+};
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.siblings = function (selector) {
+  let numberOfItems = 0;
+  let counter = 0;
+  const copyObj = Object.assign({}, this);
+
+  for (let i = 0; i < copyObj.length; i++) {
+    const arr = copyObj[i].parentNode.children;
+
+    for (let j = 0; j < arr.length; j++) {
+      if (copyObj[i] === arr[j]) {
+        continue;
+      }
+
+      this[counter] = arr[j];
+      counter++;
+    }
+
+    numberOfItems += arr.length - 1;
+  }
+
+  this.length = numberOfItems;
+  const objLength = Object.keys(this).length;
+
+  for (; numberOfItems < objLength; numberOfItems++) {
+    delete this[numberOfItems];
+    return this;
+  }
+};
+
 /***/ }),
 
 /***/ "./src/js/lib/modules/attributes.js":
@@ -467,8 +513,10 @@ $('button').click(function () {
 });
 $('div').click(function () {
   console.log($(this).index());
-});
-console.log($('div').eq(0).find('.some'));
+}); // console.log($('div').eq(2).find('.some'));
+// console.log($('.some').closest('.findmeq'));
+
+console.log($('.findme').siblings());
 
 /***/ })
 
--- src/js/main.js
+++ src/js/main.js
@@ -8,4 +8,8 @@ $('div').click(function() {
     console.log($(this).index());
 });
 
-console.log($('div').eq(0).find('.some'));
+// console.log($('div').eq(2).find('.some'));
+
+// console.log($('.some').closest('.findmeq'));
+
+console.log($('.findme').siblings());
